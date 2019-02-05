0c9848bc0b6622a503adea53df7f0aab9369b892
naveed <nksherani@outlook.com>
2019-02-05 16:37:45 +0500

    no message

---
+++ CMS/Controllers/MVCControllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers.MVCControllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        private readonly IUserSession _userSession;
+
+        public HomeController(IUserSession userSession)
+        {
+            _userSession = userSession;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var getTokenUrl = string.Format(ConfigurationManager.AppSettings["ApiBaseUri"].ToString() + "/oauth/token");
+            //LoginModel model = new LoginModel()
+            //{
+            //    Username = "SuperPowerUser",
+            //    Password = "MySuperP@ssword!"
+            //};
+            //using (HttpClient httpClient = new HttpClient())
+            //{
+            //    HttpContent content = new FormUrlEncodedContent(new[]
+            //    {
+            //        new KeyValuePair<string, string>("grant_type", "password"),
+            //        new KeyValuePair<string, string>("username", model.Username),
+            //        new KeyValuePair<string, string>("password", model.Password)
+            //    });
+
+            //    System.Net.Http.HttpResponseMessage result = httpClient.PostAsync(getTokenUrl, content).Result;
+
+            //    string resultContent = result.Content.ReadAsStringAsync().Result;
+
+            //    //var token = JsonConvert.DeserializeObject<Token>(resultContent);
+
+            //}
+            ViewBag.Username = _userSession.Username;
+            ViewBag.AccessToken = _userSession.BearerToken;
+
+            return View();
+        }
+    }
+
+
+    public interface IUserSession
+    {
+        string Username { get; }
+        string BearerToken { get; }
+    }
+
+    public class UserSession : IUserSession
+    {
+
+        public string Username
+        {
+            get { return ((ClaimsPrincipal)HttpContext.Current.User).FindFirst(ClaimTypes.Name).Value; }
+        }
+
+        public string BearerToken
+        {
+            get { return ((ClaimsPrincipal)HttpContext.Current.User).FindFirst("AcessToken").Value; }
+        }
+
+    }
+}
---
+++ CMS/Controllers/MVCControllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetIdentity.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers.MVCControllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //[Authorize]
+        public ActionResult Index()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiIzYWRiM2Y1MS04ZTE5LTQzZTUtOTk4OS05OTU0Nzg0ZjY0YzAiLCJ1bmlxdWVfbmFtZSI6IlN1cGVyUG93ZXJVc2VyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS9hY2Nlc3Njb250cm9sc2VydmljZS8yMDEwLzA3L2NsYWltcy9pZGVudGl0eXByb3ZpZGVyIjoiQVNQLk5FVCBJZGVudGl0eSIsIkFzcE5ldC5JZGVudGl0eS5TZWN1cml0eVN0YW1wIjoiNjVlOTUwMjgtZTQxZC00NWIwLWI5ZTMtYWFjMTAwZjJlYWU4Iiwicm9sZSI6WyJTdXBlckFkbWluIiwiQWRtaW4iXSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1OTgyMiIsImF1ZCI6IjQxNGUxOTI3YTM4ODRmNjhhYmM3OWY3MjgzODM3ZmQxIiwiZXhwIjoxNTA2MjAzNTE2LCJuYmYiOjE1MDYxMTcxMTZ9.WoGiiQU59Rc2RRJkEpJ0kQaKdzGnWGPraGwo5RZdfQU");
+                client.BaseAddress = new Uri( "http://localhost:55426/api/");
+                var response = client.GetAsync("Accounts/Users");
+                response.Wait();
+                var result = response.Result;
+                if(result.IsSuccessStatusCode)
+                {
+
+                }
+            }
+            return View();
+        }
+    }
+}
